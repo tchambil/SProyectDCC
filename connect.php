779b3952c4dd0e8609225b00f6c9a21a6bec9764
unknown <alejandro.lvd@gmail.com>
2014-10-09 09:45:52 -0300

    Actualizado a mysqli

--- connect.php
+++ connect.php
@@ -1,23 +1,23 @@
 <?php
-	$host = "anakena.dcc.uchile.cl";
+	echo "test";
+	$host = "localhost";
 	$user = "cc5401g2";
-	$pwd = "/buX1USo;!97";
+	$pwd = addslashes("/buX1USo;!97");
 	$dbname = "5401_1402_g2";
-	$puerto='3306';
-	$link = mysql_connect( $host, $user, $pwd ) or die( "Error de conexión: " . mysql_error() );
-	mysql_select_db($dbname,$link);
+	$puerto = "3306";
+	echo "test";
+	$link = mysqli_connect( $host, $user, $pwd ) or die( "Error de conexiÃ³n: " . mysql_error() );
+	mysqli_select_db($link, $dbname);
 	
-	if (!mysql_set_charset('utf8', $link)) {
+	if (!mysqli_set_charset($link, "utf8")) {
 			echo "Error: Unable to set the character set.\n";
 			exit;
 	}
-	
-	$sql = "SELECT *
-			FROM tpr_rol";
-	$resultado = mysql_query($sql) or die('La consulta fallo: ' . mysql_error());
-	while($res = mysql_fetch_array($resultado)){
-		echo $res[0]." ".$res[1];
+	$sql = "SELECT * FROM tpr_rol";
+	$resultado = mysqli_query($link, $sql) or die('La consulta fallo: ' . mysql_error());
+	while(($res = mysqli_fetch_assoc($resultado)) != null){
+		print_r($res);
 	}
 	
-	mysql_close($link);
+	mysqli_close($link);
 ?>
